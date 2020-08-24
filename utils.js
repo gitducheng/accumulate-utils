7cdb193bccd7d619084ce38574f706d902ff4f81
gitducheng <1147914778@qq.com>
2020-08-24 11:33:17 +0800

    add: vue bus

--- utils.js
+++ utils.js
@@ -1,13 +1,53 @@
 // 计算中英、字符数
-countStringLength(str) {
-    let zhLength = 0
-    let enLength = 0
-    for (let i = 0; i < str.length; i++) {
-        if (str.charCodeAt(i) > 127 || str.charCodeAt(i) === 94) {
-            zhLength++
-        } else {
-            enLength++
-        }
+function countStringLength(str) {
+  let zhLength = 0
+  let enLength = 0
+  for (let i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) > 127 || str.charCodeAt(i) === 94) {
+      zhLength++
+    } else {
+      enLength++
     }
-    return { zhLength, enLength }
+  }
+  return { zhLength, enLength }
+}
+
+// vue数据总线
+function VueBus(Vue) {
+  const bus = new Vue()
+
+  Object.defineProperties(bus, {
+    on: {
+      get() {
+        return this.$on.bind(this)
+      }
+    },
+    once: {
+      get() {
+        return this.$once.bind(this)
+      }
+    },
+    off: {
+      get() {
+        return this.$off.bind(this)
+      }
+    },
+    emit: {
+      get() {
+        return this.$emit.bind(this)
+      }
+    }
+  })
+
+  Object.defineProperty(Vue, 'bus', {
+    get() {
+      return bus
+    }
+  })
+
+  Object.defineProperty(Vue.prototype, '$bus', {
+    get() {
+      return bus
+    }
+  })
 }
