6e249e4dc4b82263ce0789ab2df3553d724fd04a
gitducheng <1147914778@qq.com>
2020-08-30 11:17:07 +0800

    add: 生成随机整数

--- utils.js
+++ utils.js
@@ -1,53 +1,70 @@
-// 计算中英、字符数
+/**
+ * 计算中英、字符数
+ * @param str 
+ */
 function countStringLength(str) {
-  let zhLength = 0
-  let enLength = 0
-  for (let i = 0; i < str.length; i++) {
-    if (str.charCodeAt(i) > 127 || str.charCodeAt(i) === 94) {
-      zhLength++
-    } else {
-      enLength++
+    let zhLength = 0
+    let enLength = 0
+    for (let i = 0; i < str.length; i++) {
+        if (str.charCodeAt(i) > 127 || str.charCodeAt(i) === 94) {
+            zhLength++
+        } else {
+            enLength++
+        }
     }
-  }
-  return { zhLength, enLength }
+    return { zhLength, enLength }
 }
 
-// vue数据总线
+/**
+ * vue数据总线
+ * @param Vue 
+ */
 function VueBus(Vue) {
-  const bus = new Vue()
+    const bus = new Vue()
 
-  Object.defineProperties(bus, {
-    on: {
-      get() {
-        return this.$on.bind(this)
-      }
-    },
-    once: {
-      get() {
-        return this.$once.bind(this)
-      }
-    },
-    off: {
-      get() {
-        return this.$off.bind(this)
-      }
-    },
-    emit: {
-      get() {
-        return this.$emit.bind(this)
-      }
-    }
-  })
+    Object.defineProperties(bus, {
+        on: {
+            get() {
+                return this.$on.bind(this)
+            }
+        },
+        once: {
+            get() {
+                return this.$once.bind(this)
+            }
+        },
+        off: {
+            get() {
+                return this.$off.bind(this)
+            }
+        },
+        emit: {
+            get() {
+                return this.$emit.bind(this)
+            }
+        }
+    })
 
-  Object.defineProperty(Vue, 'bus', {
-    get() {
-      return bus
-    }
-  })
+    Object.defineProperty(Vue, 'bus', {
+        get() {
+            return bus
+        }
+    })
 
-  Object.defineProperty(Vue.prototype, '$bus', {
-    get() {
-      return bus
-    }
-  })
+    Object.defineProperty(Vue.prototype, '$bus', {
+        get() {
+            return bus
+        }
+    })
+}
+
+/**
+ * 生成随机整数
+ * @param min
+ * @param max
+ */
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
 }
